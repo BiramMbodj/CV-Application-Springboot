7e4cb6991fc267482c8a8c1d72c561e089b465aa
BiramMbodj <bremsmbodj@gmail.com>
2023-03-04 05:14:27 +0000

    Implement interface 'AppCvRepository' inside new package 'ws'

---
+++ cv/src/main/java/sn/esmt/cv/dao/AppCvRepository.java
@@ -0,0 +1,9 @@
+package sn.esmt.cv.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sn.esmt.cv.entities.AppCvEntity;
+
+@Repository
+public interface AppCvRepository extends JpaRepository<AppCvEntity, Integer> {
+}
