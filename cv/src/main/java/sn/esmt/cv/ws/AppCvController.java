bd6a8f60ade5cd84d916f5ee431210255cb3304e
BiramMbodj <bremsmbodj@gmail.com>
2023-03-04 06:07:07 +0000

    Edit class 'AppCvController'

--- cv/src/main/java/sn/esmt/cv/ws/AppCvController.java
+++ cv/src/main/java/sn/esmt/cv/ws/AppCvController.java
@@ -1,12 +1,23 @@
 package sn.esmt.cv.ws;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import sn.esmt.cv.dao.AppCvRepository;
 import sn.esmt.cv.entities.AppCvEntity;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path="/cv")
 public class AppCvController {
+    private AppCvRepository appCvRepository;
 
+    @PostMapping(path = "/save")
+    public AppCvEntity save(@RequestBody AppCvEntity appCvEntity){
+        return appCvRepository.save(appCvEntity);
+    }
+
+    @GetMapping(path = "/all")
+    public List<AppCvEntity> getAll(){
+        return appCvRepository.findAll();
+    }
 }
